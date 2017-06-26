a35e170944169ffe35855388adaad7c6b233c821
Jeremy Weinstein <jeremydw@gmail.com>
2017-06-26 14:39:13 -0700

    Initial port of built-in Contentful preprocessor to extension.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name='grow-ext-contentful',
+    version='1.0.0',
+    license='MIT',
+    author='Grow Authors',
+    author_email='hello@grow.io',
+    include_package_data=False,
+    packages=[
+        'contentful_ext',
+    ],
+    install_requires=[
+        'contentful.py==0.9.3',
+    ],
+)
